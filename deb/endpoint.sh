f31563169e036bf12093739de396a81748f45c40
WallaceWilliam <volos4@ukr.net>
2020-11-06 21:27:33 +0300

    modified:   deb/endpoint.sh

--- deb/endpoint.sh
+++ deb/endpoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 apt-get install -y --no-install-recommends /deb/libfptr10_10.8.0.0_amd64-mod.deb /deb/fptr10-web-server_10.8.0.0_amd64.deb \
-&& atol-fptr-web-server --service=start \
+&& atol-fptr-web-server --service=restart \
 && tail -f /dev/null
-
